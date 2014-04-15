7deb275a44fe4b9daec3d1444e5cb8682eca8203
Chris Scheller <schelcj@pobox.com>
2014-04-15 10:11:43 -0400

    initial commit

---
+++ lib/Bundle/GotCloud.pm
@@ -0,0 +1,47 @@
+package Bundle::GotCloud;
+
+# ABSTRACT: Bundle file for GotCloud
+
+$VERSION = '0.42';
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+cpanm --mirror-only --mirror https://stratopan.com/schelcj/GotCloud/master GotCloud::Bundle
+
+=head1 CONTENTS
+
+Modern::Perl
+
+List::MoreUtils
+
+Test::Most
+
+Test::Class
+
+Test::Exception
+
+Readonly::XS
+
+Type::Tiny
+
+Moo
+
+App::Cmd
+
+Cache::File
+
+File::Slurp
+
+Parallel::ForkManager
+
+IPC::System::Simple
+
+File::Spec
+
+=head1 DESCRIPTION
+
+Dependencies for GotCloud.
